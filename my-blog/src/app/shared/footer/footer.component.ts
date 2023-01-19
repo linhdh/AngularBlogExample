04e845ad5554816950bd23e628b21c28e6ba306f
Dao Hai Linh <linhdh@live.com>
2023-01-19 14:47:19 +0700

    add articles module

--- my-blog/src/app/shared/footer/footer.component.ts
+++ my-blog/src/app/shared/footer/footer.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  currentDate = new Date();
 
+  constructor() { }
+
+  ngOnInit(): void {}
 }
